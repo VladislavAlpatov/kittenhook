00a1faf3a380486faf0bdc53fb9d185bb6bab3ae
Vladislav Alpatov <alpatof74@gmail.com>
2024-02-24 00:01:44 +0300

    update

--- source/gui/overlay/Overlay.cpp
+++ source/gui/overlay/Overlay.cpp
@@ -16,12 +16,21 @@
 #include <apex_sdk/WorldToScreen.h>
 #include <format>
 #include <unordered_map>
+#include <string>
+
 
 void TextCentered(const std::string& text, const ImVec2& pos) {
     auto textWidth   = ImGui::CalcTextSize(text.c_str()).x;
 
     ImGui::GetBackgroundDrawList()->AddText({pos.x - textWidth / 2.f, pos.y}, ImColor(0,255,0), text.c_str());
 }
+struct BorderBox
+{
+    ImVec2 m_topLeft;
+    ImVec2 m_topRight;
+    ImVec2 m_bottomLeft;
+    ImVec2 m_bottomRigh;
+};
 
 namespace ktth::overlay
 {
@@ -80,7 +89,7 @@ namespace ktth::overlay
                         continue;
 
                     auto bottom = apex_sdk::WorldToScreen(entity.GetOrigin(), 1920, 1080);
-                    auto top = apex_sdk::WorldToScreen(entity.GetBonePosition(8)+uml::Vector3{0.f,0.f,10.f},
+                    auto top = apex_sdk::WorldToScreen(entity.GetBonePosition(apex_sdk::BoneIndex::Neck)+uml::Vector3{0.f,0.f,10.f},
                                                        1920,
                                                        1080);
 
@@ -99,22 +108,15 @@ namespace ktth::overlay
 
                     auto drawList = ImGui::GetBackgroundDrawList();
 
-                    drawList->AddLine({topLeft.x, topLeft.y}, {topRight.x, topRight.y}, ImColor(255, 0,0));
-                    drawList->AddLine({bottomLeft.x, bottomLeft.y}, {bottomRight.x, bottomRight.y}, ImColor(255, 0,0));
-                    drawList->AddLine({topLeft.x, topLeft.y}, {bottomLeft.x, bottomLeft.y}, ImColor(255, 0,0));
-                    drawList->AddLine({topRight.x, topRight.y}, {bottomRight.x, bottomRight.y}, ImColor(255, 0,0));
+                    drawList->AddLine(topLeft.As<ImVec2>(), topRight.As<ImVec2>(), ImColor(255, 0,0));
+                    drawList->AddLine(bottomLeft.As<ImVec2>(), bottomRight.As<ImVec2>(), ImColor(255, 0,0));
+                    drawList->AddLine(topLeft.As<ImVec2>(), bottomLeft.As<ImVec2>(), ImColor(255, 0,0));
+                    drawList->AddLine(topRight.As<ImVec2>(), bottomRight.As<ImVec2>(), ImColor(255, 0,0));
 
-                    static std::unordered_map<int, ImColor> shieldColors =
-                    {
-                        {50, {156, 156, 156}},
-                        {75, {0, 162, 255}},
-                        {100, {105, 0, 186}},
-                        {125, {255,0,0}},
-                    };
                     const auto entShieldsMax = entity.GetMaxShieldValue();
-                    if (entShieldsMax > 0)
+                    if (entShieldsMax > 0 && entity.GetShieldValue() > 0)
                         drawList->AddText({topRight.x+5, topRight.y},
-                                          shieldColors.at(entShieldsMax),
+                                          entity.GetShieldColor().As<ImColor>(),
                                           std::format("Shield: {}/{}", entity.GetShieldValue(), entShieldsMax).c_str());
                     const auto distance = localPlayer->GetCameraPosition().DistTo(entity.GetOrigin());
                     TextCentered(std::format("[{}]", (int)(distance * 2.54f / 100.f)), {bottom->x, bottom->y});
--- source/main.cpp
+++ source/main.cpp
@@ -1,12 +1,20 @@
 #include <QApplication>
 #include <thread>
-
+#include <iostream>
 #include "gui/overlay/Overlay.h"
 #include "gui/menu/Menu.h"
 #include "hacks/Aimbot.h"
+#include "apex_sdk/MemoryManagerSinglton.h"
+#include <uml/color.h>
+#include <apex_sdk/Offsets.h>
+
 
 int main(int argc, char *argv[])
 {
+    auto& manager = apex_sdk::Memory::Get();
+    const auto val = manager.PatternScan("r5apex.exe", apex_sdk::signatures::m_bleedOutState.data());
+    setbuf(stdout, nullptr);
+    std::cout << "Pattern scan result: 0x" << std::hex << val.value_or(0);
     QApplication app(argc, argv);
     std::thread([]{ktth::overlay::Overlay().Run();}).detach();
     std::thread([]
@@ -20,5 +28,5 @@ int main(int argc, char *argv[])
     auto menu = ktth::menu::Menu();
     menu.show();
 
-    app.exec();
+    return QApplication::exec();
 }
